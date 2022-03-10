5bcc250837e41594001f6160366f0bb0544c9787
d1658999 <d1658999@gmail.com>
2022-03-11 01:10:52 +0800

    update: 1. refresh the datas 2. transfer to pivot table by PRB and FRB power by BW

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pathlib
 import pandas as pd
+import numpy as np
 
+from ltemch import mchs
+
+
+def Mch_judge(ch, mch):
+    if ch < mch:
+        return 'L'
+    elif ch> mch:
+        return 'H'
+    else:
+        return 'M'
 
 def csv2pivot_table():
     path = pathlib.Path("./")
@@ -8,22 +19,34 @@ def csv2pivot_table():
 
     for file in path.iterdir():
         if file.match('*.csv'):  # to find *csv file
+            # print(file)
             df = pd.read_csv(file, on_bad_lines='skip', skiprows=[0],
                              usecols=usecols)  # load the csv and skip the first row and the second row is the colums
             df[['Test Item', 'BW', 'Modulation']] = df['Test Item'].str.split(';',
                                                                               expand=True)  # split the Test Item by ';'
             # df['BW'] = df['Test Item'].str.split(';').str[1]
 
-            condition = (df['Test Item'].str.contains('Adjacent Channel Power') &
-                         df['Modulation'].str.contains('QPSK-PRB@0'))  # filter what we want, and this can be modified
-
             # add the channel judgement to L/M/H
+            df['channel'] = np.nan
+
+            for b in set(df.Band):
+                df.loc[df.Band==b, 'channel'] = df.loc[df.Band==b, 'Ch'].apply(Mch_judge, args=(mchs[b],))
+
 
             # this begins to transfer to pivot table
-            # pt = df.pivot_table(df[condition], index='Band', columns=['BW', 'Ch'], values=['Result'])
 
-            print(df[condition].head(50))
+            prb_pwr_condition = (df['Test Item'].str.contains('Adjacent Channel Power') &
+                         df['Modulation'].str.contains('QPSK-PRB@0'))  # filter what we want, and this can be modified
+            frb_pwr_condition = (df['Test Item'].str.contains('Adjacent Channel Power') &
+                                 df['Modulation'].str.contains('QPSK-FRB'))  # filter what we want, and this can be modified
 
+            df_prb_pwr = df[prb_pwr_condition]
+            df_frb_pwr = df[frb_pwr_condition]
+
+            pt_prb_pwr = df_prb_pwr.pivot_table(index='Band', columns=['BW', 'channel'], values='Result', aggfunc='max')
+            pt_frb_pwr = df_frb_pwr.pivot_table(index='Band', columns=['BW', 'channel'], values='Result', aggfunc='max')
+            #print(df_frb_pwr[['Ch', 'Result']].head(20))
+            print(pt_frb_pwr)
 
 def main():
     csv2pivot_table()
@@ -31,4 +54,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
